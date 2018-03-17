ce07996dd62cbd0332b10a3ace934eff76940b80
bxcar <malanchukdima@mail.ru>
2018-03-17 13:59:17 +0200

    before html

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglifyjs');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
 });
 
 gulp.task('browser-sync', function () {
@@ -18,6 +30,8 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function () {
+gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function () {
     gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
