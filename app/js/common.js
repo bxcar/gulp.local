fca6072756d9380c544579dc1fc94e295c9cac1d
bxcar <malanchukdima@mail.ru>
2018-03-28 16:29:58 +0300

    slider on photo and main pages done

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,53 @@
-$(".trigger").click(function(event) {
-    event.preventDefault();
-    $(".modal-common").toggleClass("show-modal");
-});
+$(document).ready(function () {
+    $(".trigger").click(function (event) {
+        event.preventDefault();
+        $(".modal-common").toggleClass("show-modal");
+    });
 
-$("#simple-form").submit(function(event) {
-    event.preventDefault();
-    $(".modal-common.thanks").toggleClass("show-modal");
-});
+    $("#simple-form").submit(function (event) {
+        event.preventDefault();
+        $(".modal-common.thanks").toggleClass("show-modal");
+    });
 
-$(".close-button").click(function(event) {
-    event.preventDefault();
-    $(".modal-common").toggleClass("show-modal");
+    $(".close-button").click(function (event) {
+        event.preventDefault();
+        $(".modal-common").toggleClass("show-modal");
+    });
+
+    $("#owl-6").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["<img src=\"img/photo-single-left-arrow-min.png\">", "<img src=\"img/photo-single-right-arrow-min.png\">"],
+        items: 6
+    });
+
+    $("#owl-main-page-slider").owlCarousel({
+        loop: true,
+        nav: true,
+        navText: ["<img src=\"img/main-page-left-arrow.png\">", "<img src=\"img/main-page-right-arrow.png\">"],
+        items: 1,
+        singleItem: true,
+        dots: true,
+        dotsContainer: '#carousel-custom-dots'
+    });
+
+    $('.owl-dot').click(function () {
+        $("#owl-main-page-slider").trigger('to.owl.carousel', [$(this).index(), 300]);
+    });
+
+    /*var indexToRemove = 6;
+     $("#owl-6").trigger('remove.owl.carousel', [indexToRemove]).trigger('refresh.owl.carousel');*/
+
+    $("#owl-1").owlCarousel({
+        navText: ["<img src=\"img/photo-single-left-arrow.png\">", "<img src=\"img/photo-single-right-arrow.png\">"],
+        loop: true,
+        nav: true,
+        items: 1,
+        thumbs: true,
+        thumbsPrerendered: true
+        // thumbImage: true,
+        // thumbContainerClass: 'owl-thumbs',
+        // thumbItemClass: 'owl-thumb-item'
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ gulp.task('sass', function () {
 gulp.task('scripts', function () {
     return gulp.src([
         'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+        'app/libs/owlcarousel/js/owl.carousel.min.js',
+        'app/libs/owlcarousel/js/owl.carousel2.thumbs.min.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
